728c2a0827ce3ea4df418a6a7715575378ba3da7
Hiro <laciferin@gmail.com>
2023-07-30 19:27:40 +0530

    wip: auth

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,9 +1,9 @@
-import { Args, Context, Mutation, Resolver } from '@nestjs/graphql'
-import { AuthService } from './auth.service'
-import { AuthController } from './auth.controller'
-import { GraphqlContext } from '../app.dto'
-import { AuthDto } from './dto'
-import { AuthInput } from '../graphql'
+import {Args, Context, Mutation, Resolver} from '@nestjs/graphql'
+import {AuthService} from './auth.service'
+import {AuthController} from './auth.controller'
+import {GraphqlContext} from '../app.dto'
+import {AuthDto} from './dto'
+import {AuthInput} from '../graphql'
 
 @Resolver('Auth')
 export class AuthResolver {
@@ -12,12 +12,16 @@ export class AuthResolver {
     private readonly authController: AuthController,
   ) {}
 
-  @Mutation('signIn')
-  async signIn(@Args('data') dto: AuthDto, @Context() ctx: GraphqlContext) {
-    console.debug({ dto })
+  @Mutation('signUp')
+  signUp(@Args('data') dto: AuthDto) {
+    console.debug({dto})
 
-    // ctx.res.cookie('some-cookie', 'some-value')
-    // ctx.res.header('some-header', 'some-header')
+    return this.authController.signup(dto as AuthInput)
+  }
+
+  @Mutation('signIn')
+  signIn(@Args('data') dto: AuthDto, @Context() ctx: GraphqlContext) {
+    console.debug({ dto })
 
     return this.authController.signin(dto as AuthInput, ctx.res)
   }
