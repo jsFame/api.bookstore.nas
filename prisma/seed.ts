9e308979088a434c8dc02338016566d112d3fb76
Hiro <laciferin@gmail.com>
2023-07-30 19:35:47 +0530

    migrations:

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,63 +1,81 @@
-import { PrismaClient } from '@prisma/client'
-import { date } from 'joi'
+import {PrismaClient} from '@prisma/client'
+import * as argon from 'argon2'
+import {AuthDto} from '../src/auth/dto'
+import moment from 'moment'
+
 const prisma = new PrismaClient()
 
+async function create_user(dto: AuthDto) {
+  const hash = await argon.hash(dto.password)
+  const user = await prisma.user.create({
+    data: {
+      email: dto.email,
+      hash: hash,
+    },
+  })
+  console.log({created: user})
+  return user
+}
+
+function addDays(date: Date, days: number): Date {
+  date.setDate(date.getDate() + days)
+  return date
+}
+
 async function main() {
-  await prisma.wallet.deleteMany()
-  const wallet = await prisma.wallet.create({
+  await prisma.user.deleteMany()
+
+  const user1 = await create_user({
+    email: 'dean@nas.edu',
+    password: 'dean@Testing#',
+  })
+  const user2 = await create_user({
+    email: 'hiro@student.nas.edu',
+    password: 'hiro@Testing#',
+  })
+
+  const event = await prisma.event.create({
     data: {
-      address: 'of1ExCK6cWFxCxtUGw2civp4RKejVSQ5EzntJPntDTP',
+      title: '20 Minute catchup',
+      hostId: user1.id,
+      duration: 20,
     },
   })
-  const todo_board = await prisma.board.create({
+  console.log({event})
+
+  const startTime = moment('12:00', 'HH:mm')
+  const endTime = startTime.clone().add(event.duration, 'minutes')
+
+  console.log({startTime, endTime})
+
+  const timeSlot = await prisma.timeSlot.create({
     data: {
-      walletId: wallet.id,
-      isActive: true,
-      name: 'Taskmon.ai',
+      eventId: event.id,
+      available: true,
+      startTime: startTime.toDate(),
+      endTime: endTime.toDate(),
     },
   })
 
-  const todo_cln = await prisma.column.create({
-    data: {
-      name: 'TODO',
-      boardId: todo_board.id,
-    },
-  })
+  console.log({timeSlot})
 
-  const scaffold_task = await prisma.task.create({
-    data: {
-      title: 'Scaffold: nestjs',
-      description: `
-      - Support /auth endpoint,
-      `.trim(),
-      columnId: todo_cln.id,
-    },
-  })
-
-  const todo_task = await prisma.task.create({
-    data: {
-      title: 'Scaffold: graphql',
-      description: `
-      - Support /graphql endpoint,
-      - Will be be able query all the tasks,
-      - Create Mutations to support CRUD on tasks,boards, subtasks
-      `.trim(),
-      columnId: todo_cln.id,
-    },
-  })
-  const subtasks_scaffold = await prisma.subtask.createMany({
+  const today = new Date()
+  const calendarInvite = await prisma.calendar.createMany({
     data: [
-      { title: 'integrate apolo server', taskId: todo_task.id },
-      { title: 'nest g resource task', taskId: todo_task.id, isCompleted: true },
       {
-        title: 'go with schema first resource creation',
-        taskId: todo_task.id,
-        isCompleted: true,
+        timeSlotId: timeSlot.id,
+        date: new Date(today.getTime() + 1000 * 60 * 60 * 24),
+        guestId: user2.id,
+      },
+      {
+        timeSlotId: timeSlot.id,
+        date: new Date(today.getTime() + 1000 * 60 * 60 * 24 * 2),
+        guestId: user2.id,
       },
     ],
   })
 
-  console.log({ wallet, board: todo_board, todo_cln, todo_task, subtasks_scaffold })
+  console.log({calendarInvite})
 }
 
 main()
