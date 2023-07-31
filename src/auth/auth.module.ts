80236d9f8e8afba2a9fbaafe8253b1cafe539e79
Hiro <laciferin@gmail.com>
2023-08-01 02:43:46 +0530

    graphql: apis fixed
    Took 1 minute

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { AuthService } from './auth.service'
 import { JwtModule } from '@nestjs/jwt'
 import { PrismaModule } from '../prisma/prisma.module'
 import { JwtStrategy } from './strategy'
+import { AuthResolver } from './auth.resolver'
 
 @Module({
   imports: [JwtModule.register({}), PrismaModule],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, AuthResolver, AuthController],
 })
 export class AuthModule {}
