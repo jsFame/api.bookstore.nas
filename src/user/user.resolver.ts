9e3b3066c7a4ea1f91aa78e219ed434131b08f5b
Hiro <laciferin@gmail.com>
2023-08-01 03:31:15 +0530

    resolver:
    Took 12 minutes

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,6 +1,4 @@
 import { Query, Resolver } from '@nestjs/graphql'
-import { PrismaService } from '../prisma/prisma.service'
-import { UserService } from './user.service'
 import { UserController } from './user.controller'
 import { GetUserGraphql } from '../auth/decorator'
 import { UseGuards } from '@nestjs/common'
@@ -9,11 +7,7 @@ import { GqlAuthGuard } from '../auth/guard'
 @UseGuards(GqlAuthGuard)
 @Resolver('User')
 export class UserResolver {
-  constructor(
-    private readonly prisma: PrismaService,
-    private readonly userService: UserService,
-    private readonly userController: UserController,
-  ) {}
+  constructor(private readonly userController: UserController) {}
 
   @Query('me')
   me(@GetUserGraphql('userId') userId: number) {
